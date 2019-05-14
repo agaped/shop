660583207f3cf41acf979062529b709555626978
agaped <aga.pedlewska@gmail.com>
2019-05-14 19:19:56 +0200

    Products are visible on the home page

---
+++ backend/src/main/java/com/onlineshop/shop/dto/ProductDto.java
@@ -0,0 +1,73 @@
+package com.onlineshop.shop.dto;
+
+import java.math.BigDecimal;
+
+public class ProductDto {
+
+    private int id;
+    private String name;
+    private String producer;
+    private BigDecimal price;
+    private String description;
+    private String category;
+    private String url;
+
+    public ProductDto() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
---
+++ backend/src/main/java/com/onlineshop/shop/model/Img.java
@@ -0,0 +1,48 @@
+package com.onlineshop.shop.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Img {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id_img")
+    private int id;
+
+    private String url;
+
+    @OneToOne(mappedBy = "img")
+    private Product product;
+
+    public Img() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
+}
